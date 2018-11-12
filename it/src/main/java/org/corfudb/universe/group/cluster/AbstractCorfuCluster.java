0a977f407345800591e20410c1eba4355a26260c
Edilmo Palencia <edilmo@gmail.com>
2018-11-12 07:34:03 -0800

    Longevity Test implementation Implementation based in 5 new concepts: Events, Operators, Rules, State and Dynamic.

--- it/src/main/java/org/corfudb/universe/group/cluster/AbstractCorfuCluster.java
+++ it/src/main/java/org/corfudb/universe/group/cluster/AbstractCorfuCluster.java
@@ -9,6 +9,7 @@ import org.corfudb.universe.group.Group;
 import org.corfudb.universe.node.Node;
 import org.corfudb.universe.node.Node.NodeParams;
 import org.corfudb.universe.node.NodeException;
+import org.corfudb.universe.node.client.ClientParams;
 import org.corfudb.universe.node.client.LocalCorfuClient;
 import org.corfudb.universe.node.server.CorfuServer;
 import org.corfudb.universe.node.server.CorfuServerParams;
@@ -163,6 +164,7 @@ public abstract class AbstractCorfuCluster<P extends CorfuClusterParams, U exten
     @Override
     public LocalCorfuClient getLocalCorfuClient() {
         return LocalCorfuClient.builder()
+                .params(ClientParams.builder().build())
                 .serverEndpoints(getClusterLayoutServers())
                 .build()
                 .deploy();
