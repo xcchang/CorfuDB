406fd248efc26aaf294bb8a4451b4528f2f90f61
maxi <maxi@vmware.com>
2020-04-24 16:38:41 -0700

    Try out with grpc.

---
+++ log-replication/src/main/java/org/corfudb/logreplication/grpc/LogReplicationRPCServer.java
@@ -0,0 +1,8 @@
+package org.corfudb.logreplication.grpc;
+
+//import org.corfudb.infrastructure.logreplication.LogReplicationAdapter;
+public class LogReplicationRPCServer {
+//public class LogReplicationRPCServer extends LogReplicationAdapter.LogreplicationAdapterImplBase {
+    Character a = new Character('a');
+    Character.isLowercase("a")
+}
